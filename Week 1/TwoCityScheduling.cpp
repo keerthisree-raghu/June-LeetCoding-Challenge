6f423ed3889cf82f2d9c50d92b77e451cb137b7f
Keerthisree <51854360+keerthx@users.noreply.github.com>
2020-06-08 21:25:40 +0530

    Week 1 - 01/06/20 to 07/06/20

--- Week 1/TwoCityScheduling.cpp
+++ Week 1/TwoCityScheduling.cpp
@@ -1,7 +1,7 @@
 // TWO CITY SCHEDULING
 
 /*  PROBLEM:
-    here are 2N people a company is planning to interview. The cost of flying the 
+    There are 2N people a company is planning to interview. The cost of flying the 
     i-th person to city A is costs[i][0], and the cost of flying the i-th person 
     to city B is costs[i][1].
 
