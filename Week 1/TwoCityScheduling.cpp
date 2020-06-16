7955ca9ac0dbf9b0a3d3f10654613dd2b1350905
Keerthisree <raghu.keerthisree@gmail.com>
2020-06-16 09:33:32 +0530

    Week 1 - 01/06/20 to 08/06/20

--- Week 1/TwoCityScheduling.cpp
+++ Week 1/TwoCityScheduling.cpp
@@ -7,6 +7,11 @@
 
     Return the minimum cost to fly every person to a city such that exactly N people 
     arrive in each city.
+
+    Note:
+    1 <= costs.length <= 100
+    It is guaranteed that costs.length is even.
+    1 <= costs[i][0], costs[i][1] <= 1000
 */
 
 /*  EXAMPLE:
